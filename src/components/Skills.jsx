be09440af1f21fa95f671ea343c1c50a974e7dea
Tojima689 <tojima.ryuta@headwaters.co.jp>
2024-07-02 17:00:57 +0900

    Update skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,4 +1,49 @@
+import { CircularProgressbar } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
+import { useEffect, useReducer } from "react";
+import axios from "axios";
+import { skillReducer, initialState, actionTypes } from "../reducers/skillReducer";
+import { requestStates } from "../constants";
+
 export const Skills = () => {
+    const [state, dispatch] = useReducer(skillReducer, initialState);
+
+    useEffect(
+        () => {
+            dispatch({ type: actionTypes.fetch });
+            axios.get('https://api.github.com/users/Tojima689/repos')
+                .then((response) => {
+                    const languageList = response.data.map(res => res.language);
+                    const countedLanguageList = generateLanguageCountObj(languageList);
+                    dispatch({ type: actionTypes.success, payload: { languageList: countedLanguageList } });
+                }).catch(() => {
+                    dispatch({ type: actionTypes.error });
+                });
+        }, []);
+
+    const generateLanguageCountObj = (allLanguageList) => {
+        const notNullLanguageList = allLanguageList.filter(language => language != null);
+        const uniqueLanguageList = [...new Set(notNullLanguageList)];
+
+        return uniqueLanguageList.map(item => {
+            return {
+                language: item,
+                count: allLanguageList.filter(language => language === item).length
+            }
+        })
+    };
+
+    const convertConutToPercentage = (count) => {
+        if (count > 10) {
+            return 100;
+        }
+        return count * 10;
+    }
+
+    const sortedLanguageList = () => (
+        state.languageList.sort((firstLang, nextLang) => nextLang.count - firstLang.count)
+    )
+
     return (
         <div id="skills">
             <div className="container">
@@ -6,7 +51,26 @@ export const Skills = () => {
                     <h2>Skills</h2>
                 </div>
                 <div className="skills-container">
-
+                    {
+                        state.requestState === requestStates.loading && (
+                            <p className="description">取得中...</p>
+                        )
+                    }
+                    {
+                        state.requestState === requestStates.success && (
+                            sortedLanguageList().map((item, index) => (
+                                <div className="skill-item" key={index}>
+                                    <p className="description"><strong>{item.language}</strong></p>
+                                    <CircularProgressbar value={convertConutToPercentage(item.count)} text={`${convertConutToPercentage(item.count)}%`} />
+                                </div>
+                            ))
+                        )
+                    }
+                    {
+                        state.requestState === requestStates.error && (
+                            <p className="description">エラーが発生しました</p>
+                        )
+                    }
                 </div>
             </div>
         </div>
