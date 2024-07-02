ae0f6eb1b3e8e35b35d893dae8a28e16a98667c6
Tojima689 <tojima.ryuta@headwaters.co.jp>
2024-07-02 15:00:57 +0900

    Update Skills and Contact

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-// import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import { Header } from './components/Header';
+import { Skills } from './components/Skills';
+import { Contact } from './components/Contact';
 
 function App() {
   return (
-    <Header />
+    // 親要素が1つである必要があるため、<React.Fragment></React.Fragment>または<></>で囲む
+    <React.Fragment>
+      <Header />
+      <Skills />
+      <Contact />
+    </React.Fragment>
   );
 }
 
